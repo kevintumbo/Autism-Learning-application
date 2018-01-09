349fa146dd3043b2b7aefd1c5cefd8ebeb444ae9
Kevin <kevin.tumbo39@gmail.com>
2018-01-09 08:18:38 +0300

    feat(syllabus): create syllabus screen
    - create screen
    - add stylesheet
    - add sqlite
    
    [pending]

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import { Navigation } from 'react-native-navigation';
 
-import LoginScreen  from './src/screens/auth/login'
-import RegisterScreen from './src/screens/auth/register'
+import LoginScreen  from './src/screens/auth/login';
+import RegisterScreen from './src/screens/auth/register';
+import SyllabusScreen from './src/screens/syllabus/syllabus';
 
 // registration of screens
-Navigation.registerComponent("AutismApplication.LoginScreen", () => LoginScreen)
-Navigation.registerComponent("AutismApplication.RegisterScreen", () => RegisterScreen)
+Navigation.registerComponent("AutismApplication.LoginScreen", () => LoginScreen);
+Navigation.registerComponent("AutismApplication.RegisterScreen", () => RegisterScreen);
+Navigation.registerComponent("AutismApplication.SyllabusScreen", () => SyllabusScreen);
 
 // Start the app
 
 Navigation.startSingleScreenApp({
     screen: {
-        screen: "AutismApplication.RegisterScreen"
+        screen: "AutismApplication.SyllabusScreen"
     },
 });
